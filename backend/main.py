06276c2b3be07e50550684db52f91e9cc4774eb6
lebrancconvas <lebrancconvas@gmail.com>
2023-06-06 10:56:58 +0700

    ✨ feat: add get classes

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,13 @@ import uvicorn
 
 app = FastAPI()
 
+classes = [] 
+
 @app.get("/")
 async def index():
   return {"message": "Hello to MR Quest API."}
 
-
+# Get Classes
+@app.get("/api/v1/classes")
+async def get_classes():
+  return {"data": classes}
